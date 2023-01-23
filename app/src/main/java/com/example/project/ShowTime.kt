233a7e68abae465dbe6a30506e0c5df305138e10
Jayesh7046 <tahilianij11@gmail.com>
2023-01-23 13:13:57 +0530

    23/1/2023 movie design complete

--- app/src/main/java/com/example/project/ShowTime.kt
+++ app/src/main/java/com/example/project/ShowTime.kt
@@ -4,14 +4,17 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import com.harrywhewell.scrolldatepicker.DayScrollDatePicker
 import com.harrywhewell.scrolldatepicker.OnDateSelectedListener
 import java.util.*
 
 class ShowTime : AppCompatActivity()
 {
-    private lateinit var mPicker : DayScrollDatePicker
+    private lateinit var dayPicker: DayScrollDatePicker
+    private lateinit var selectDate: String
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?)
@@ -19,18 +22,14 @@ class ShowTime : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_time)
 
-//        mPicker = findViewById(R.id.day_date_picker)
-//
-//        mPicker.getSelectedDate(object : OnDateSelectedListener
-//        {
-//            override fun onDateSelected(@Nullable date: Date?)
-//            {
-//                if (date != null)
-//                {
-//                    // do something with selected date
-//                }
-//            }
-//        })
+        val c = Calendar.getInstance()
+
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        dayPicker = findViewById(R.id.day_date_picker)
+        dayPicker.setStartDate(day, month+1, year)
 
         findViewById<Button>(R.id.btn9am).let {
             it.setOnClickListener {
